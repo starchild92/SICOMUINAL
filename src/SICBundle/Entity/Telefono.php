a0d2a7c032adcb944ccbf2e3174cf2aacaefe1ca
Luis Pérez <lapv1992@gmail.com>
2016-06-21 21:05:04 -0400

    Entidad: Telefono  - Codigo  - Número

---
+++ src/SICBundle/Entity/Telefono.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace SICBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Telefono
+ *
+ * @ORM\Table(name="telefono")
+ * @ORM\Entity(repositoryClass="SICBundle\Repository\TelefonoRepository")
+ */
+class Telefono
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="codigo", type="integer")
+     */
+    private $codigo;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numero", type="integer")
+     */
+    private $numero;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set codigo
+     *
+     * @param integer $codigo
+     * @return Telefono
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    /**
+     * Get codigo
+     *
+     * @return integer 
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     * @return Telefono
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return integer 
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+}
